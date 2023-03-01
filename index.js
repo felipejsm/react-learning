63d0276b6a61a9dfe68243e32618bb09b0a00fe7
felipejsm <felipejsm41@gmail.com>
2023-02-28 21:14:00 -0300

    still static

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-ReactDOM.render(<ul><li>First</li><li>Second</li></ul>, document.getElementById("root"));
+const page = (
+    <nav className="navbar">
+        <ul className="navbar-list">
+            <li className="navbar-item"><a href="#" className="navbar-link">Home</a></li>
+            <li className="navbar-item"><a href="#" className="navbar-link">Sobre</a></li>
+            <li className="navbar-item"><a href="#" className="navbar-link">Serviços</a></li>
+            <li className="navbar-item"><a href="#" className="navbar-link">Contato</a></li>
+        </ul>
+    </nav>
+);
+
+ReactDOM.render(
+    page,
+    document.getElementById("root")
+);
